9a1b2fab68ccd8274e8046d7f57684982430de0a
Deependra Mishra <deependra.mishra@talentica.com>
2018-07-25 23:25:29 +0530

    Adding default values & validations for all params

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 /**
- * @param {string} message message log before starting the countdown
- * @param {number} T start countdown from T * 
- * @param {function} logger logger function
+ * log initial message then start with the countdown.
+ * 
+ * **All params are optional with default values.
+ * 
+ * @param {string} [message=Starting] message log before starting the countdown
+ * @param {number} [T=5] start countdown from T
+ * @param {function} [logger=console.log] logger function
  */
-const countdown = (message, T, logger) => {
+const countdown = (message='Starting', T=5, logger=console.log) => {
   return new Promise((resolve, reject) => {
+    if(typeof message != 'string' || typeof T != 'number' || T <= 0 || typeof logger != 'function') {
+        resolve()
+        return
+    }
+
     logger(`${message} in ${T} secs...`)
     let counterInterval = setInterval(function () {
       if (T === 0) {
