5eb4d003dde229b74dd8fb9c0eb64852ecd1e902
Deependra Mishra <deependra.mishra@talentica.com>
2018-07-27 23:47:46 +0530

    Adding styling to T-Minus log & changing default countdown start to 10

---
+++ colors.js
@@ -0,0 +1,18 @@
+const colors = {
+    Reset: "\x1b[0m",
+    Bright: "\x1b[1m",
+    fg: {
+     Red: "\x1b[31m",
+     Green: "\x1b[32m",
+     White: "\x1b[37m",
+     Crimson: "\x1b[38m"
+    },
+    bg: {
+     Red: "\x1b[41m",
+     Green: "\x1b[42m",
+     White: "\x1b[47m",
+     Crimson: "\x1b[48m"
+    }
+   };
+
+   module.exports = colors;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const colors =  require('./colors')
+
 /**
  * log initial message then start with the countdown.
  * 
@@ -7,7 +9,7 @@
  * @param {number} [T=5] start countdown from T
  * 
  */
-const countdown = (message='Starting', T=5) => {
+const countdown = (message='Starting', T=10) => {
   return new Promise((resolve, reject) => {
     if(typeof message != 'string' || typeof T != 'number' || T <= 0) {
         resolve()
@@ -17,12 +19,12 @@ const countdown = (message='Starting', T=5) => {
     let counterInterval = setInterval(function () {
       if (T === 0) {
         clearInterval(counterInterval)
-        console.log(`\r${message}                `);
+        console.log(`\r${message}\t\t`, colors.Reset);
         resolve()
         return
       }
 
-      process.stdout.write(`\r${message} in T-${T}`);
+      process.stdout.write(`\r${colors.Bright}${message} in ${colors.fg.White}${colors.bg.Red} T-${T} ${colors.Reset}`);
       T--
     }, 1000)
   })
